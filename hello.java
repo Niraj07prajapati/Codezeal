bcfe7a218abc2f5c6dce7749aab1104bb07106fd
Niraj prajapati <prajapatiniraj19@gmail.com>
2023-04-22 23:19:22 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,19 @@
+class hello{
+    public static void main(String[] args) {
+       // System.out.println("Hello world");
+       // boolean ask;
+        //float salary =8799.2F;
+        //System.out.println(salary);
+        String x = "AB C";
+        String y = "Hello world";
+        String z = "first_come";
+        String ind = "I Love My#INDIA";
+        System.out.println(ind.split(" ")[0]);
+        System.out.println(ind.split(" ")[1]);
+        String t = ind.split(" ")[2];
+        System.out.println(t.split("#")[0]);
+        System.out.println(ind.split("#")[1]);
+    
+       
+    }
+}
